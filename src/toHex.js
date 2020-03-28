4b099540553a4d463474763742f32460e2a7792e
Tetiana Zakharova <kazakova.tanya@gmail.com>
2020-03-28 22:53:35 +0200

    Solution

--- src/toHex.js
+++ src/toHex.js
@@ -20,7 +20,40 @@
  * @return {string}
  */
 function toHex(value) {
-  // write code here
+  let result = '';
+  let number = value;
+
+  if (number === 0) {
+    return '0';
+  }
+
+  while (number > 0) {
+    const rest = number % 16;
+    let hex;
+
+    number = Math.trunc(number / 16);
+
+    switch (rest) {
+      case 10: hex = 'a';
+        break;
+      case 11: hex = 'b';
+        break;
+      case 12: hex = 'c';
+        break;
+      case 13: hex = 'd';
+        break;
+      case 14: hex = 'e';
+        break;
+      case 15: hex = 'f';
+        break;
+      default: hex = rest;
+        break;
+    }
+
+    result = hex + result;
+  }
+
+  return result;
 }
 
 module.exports = toHex;
